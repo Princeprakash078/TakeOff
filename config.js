9f5b9e1a37166a8765c4b194b3738e73e07630a1
Prince Prakash <72178641+Princeprakash078@users.noreply.github.com>
2026-03-09 19:38:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,4 @@ Change this to your EC2 public IP when deploying
 Example:
 const API_BASE = "http://13.232.xxx.xxx"
 */
-
-const API_BASE = "http://YOUR_EC2_PUBLIC_IP"
+const API_BASE = "http://15.206.94.54"
